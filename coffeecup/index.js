e8d227d13bf607eacc979227d08d8797db3635a6
Kirill <k.makrutin@gmail.com>
2019-07-22 22:51:20 +0300

    Added WebSocket package

--- coffeecup/index.js
+++ coffeecup/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const WebSocket = require("ws");
 const baristaRouter = require("./routes/barista");
 const errorRouter = require("./routes/error");
 
@@ -18,4 +19,22 @@ app.use("/favicon.ico", (req, res) => {
 // routes to error page if request was unhadled
 app.use(errorRouter);
 
-app.listen(3333);
+const server = app.listen(3333);
+
+// https://www.npmjs.com/package/ws
+// see different ways how to connect https://stackoverflow.com/a/17697134
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", ws => {
+  console.log("Websocket connection established");
+
+  //connection is up, let's add a simple simple event
+  ws.on("message", message => {
+    //log the received message and send it back to the client
+    console.log(`received: ${message}`);
+    ws.send(`Hello, you sent -> ${message}`);
+  });
+
+  //send immediatly a feedback to the incoming connection
+  ws.send("Hello there, I am a WebSocket server");
+});
